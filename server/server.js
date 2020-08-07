00fc72b62c48d5cd0b50408e23541157c7181155
chenjianrui <sky75217@gmail.com>
2020-08-07 22:44:28 +0800

    安裝初始化

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+require('dotenv').config({
+  path: './config/index.env'
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('test page')
+})
+
+app.use((req, res) => {
+  res.status(404).json({
+    msg: 'Page not founded'
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
