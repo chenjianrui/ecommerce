139d5f49faa15c035429b007a8b11a9807bfdfe3
chenjianrui <sky75217@gmail.com>
2020-08-08 13:33:20 +0800

    add User Login

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -63,4 +63,53 @@ router.post('/register', [
   }
 })
 
+router.post('/login', [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists()
+], async (req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    res.status(400).json({
+      errors: errors.array()
+    })
+  }
+
+  const {email, password} = req.body
+
+  try {
+    let user = await User.findOne({ email })
+    // 當 user 不存在 DB
+    if(!user){
+      return res.status(400).json({
+        errors: [{
+          msg: 'Invalid credentials'
+        }]
+      })
+    }
+    // 驗證密碼是否跟 DB 相同
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+      return res.status(400).json({
+        errors: [{
+          msg: 'Invalid credentials'
+        }]
+      })
+    }
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+    jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: 360000
+    }, (err, token) => {
+      if(err) throw err
+      res.json({ token })
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Server error')
+  }
+})
+
 module.exports = router
