0a29566a1e588c310c570dd8114f14e25c0254a5
chenjianrui <sky75217@gmail.com>
2020-08-09 22:19:19 +0800

    實作有 Token 的話會發出 get user info API

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux'
 import store from './data/store'
 
 import Routes from './routes'
+import { setAuthToken } from './helpers/setAuthToken'
+import { loadUser } from './data/reducers/auth'
+
+if(localStorage.token){
+  setAuthToken(localStorage.token)
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
   return (
     <Provider store={store}>
       <Routes />
--- client/src/data/reducers/auth.js
+++ client/src/data/reducers/auth.js
@@ -2,10 +2,13 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 
 import { URLDevelopment } from '../../helpers/URL'
+import { setAuthToken } from '../../helpers/setAuthToken'
 
 // Types
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const REGISTER_FAIL = 'REGISTER_FAIL'
+const USER_LOADED = 'USER_LOADED'
+const AUTH_ERROR = 'AUTH_ERROR'
 
 
 // initial state
@@ -22,6 +25,13 @@ const initialState = {
 export default (state = initialState, action) => {
   const { type, payload } = action
   switch(type){
+    case USER_LOADED:
+      return {
+        ...state,
+        user: payload,
+        isAuthenticated: true,
+        loading: false
+      }
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token)
       return {
@@ -31,6 +41,7 @@ export default (state = initialState, action) => {
         loading: false,
       }
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token')
       return {
         ...state,
@@ -44,6 +55,25 @@ export default (state = initialState, action) => {
 }
 
 // actions
+
+export const loadUser = () => async dispatch => {
+  /** 
+   * Method setAuthToken
+   * 除了註冊外，其它的請求都需要在 Header 裡帶著 Token，所以 axios default 在 local storage 裡有 token 的 
+   * 話就會帶上去 Header
+   */
+  if(localStorage.token){
+    setAuthToken(localStorage.token)
+  }
+
+  try {
+    const res = await axios.get(`${URLDevelopment}/api/user`)
+    dispatch({ type: USER_LOADED, payload: res.data })
+  } catch (error) {
+    console.log(error.message)
+    dispatch({ type: AUTH_ERROR })
+  }
+}
 
 export const register = ({name, email, password}) => async dispatch => {
   const config = {
